6e7882eea44abf97f1e5ead780128e8dab1c956e
Bashido88 <80066734+Bashido88@users.noreply.github.com>
2021-03-04 20:32:14 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';	
 
 const firebaseConfig = {	
-    apiKey: "AIzaSyAkxD7psSCUhxcM417pcDNq0hKe1NHX1y4",
-    authDomain: "chat-b1687.firebaseapp.com",
-    projectId: "chat-b1687",
-    storageBucket: "chat-b1687.appspot.com",
-    messagingSenderId: "230824056148",
-    appId: "1:230824056148:web:c70f1c84490a817c41e4c1"
+    apiKey: "${VAR_API}",
+    authDomain: "${VAR_AUTH}",
+    projectId: "${VAR_PROJECT}",
+    storageBucket: "${VAR_STORAGE}",
+    messagingSenderId: "${VAR_MESS}",
+    appId: "${VAR_APP}"
 };	
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);	
